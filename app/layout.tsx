ba191b8eba334c4591002ba1691817f0c73c1dd6
Jan Cernik <jancernik12@gmail.com>
2026-03-29 22:20:35 -0300

    Use Roboto as the main font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next"
 
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  fallback: ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "sans-serif"],
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+})
 import NextTopLoader from "nextjs-toploader"
 import { ReactNode } from "react"
 
@@ -20,7 +27,7 @@ export default function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html className="bg-black" lang="en" suppressHydrationWarning>
+    <html className={`bg-black ${roboto.className}`} lang="en" suppressHydrationWarning>
       <body className="bg-black text-[#ededed]">
         <NextTopLoader
           color="#ededed"
